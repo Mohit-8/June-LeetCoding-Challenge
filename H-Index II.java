f9e11a3bec994d512c73b54e212e393120ab27af
Mohit <58105910+Mohit-8@users.noreply.github.com>
2020-06-19 23:59:22 +0530

    solution added

---
+++ H-Index II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int hIndex=0;
+        int start=0;
+        int end=citations.length-1;    
+        int len=citations.length;
+        
+        while(start<=end) {
+            int mid = start + (end-start)/2;
+            if(citations[mid]<=len-mid) {
+                hIndex=hIndex<citations[mid]?citations[mid]:hIndex;
+                start=mid+1;
+            } else {
+                hIndex=hIndex<len-mid?len-mid:hIndex;
+                end=mid-1;
+            }
+        }
+        return hIndex;
+    }
+}
